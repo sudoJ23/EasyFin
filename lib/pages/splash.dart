2140702a78a106a892911aa0c1b5f6d638915c4f
Hanggoro <65228679+Zinc4@users.noreply.github.com>
2024-01-12 09:39:23 +0700

    Create splash.dart

---
+++ lib/pages/splash.dart
@@ -0,0 +1,100 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:easyfin/commons/socket_helper.dart';
+import 'package:easyfin/entity/User.dart';
+
+import '../entity/Accounts.dart';
+
+class SplashScreen extends StatefulWidget {
+  final Future<FirebaseApp> initialization;
+  const SplashScreen({super.key, required this.initialization});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  String status = "connecting to socket";
+
+  @override
+  void initState() {
+    super.initState();
+    widget.initialization.then((_) async => {
+          await SocketManager.shared.initSocket(),
+          setState(() {
+            status = "connected";
+          }),
+          if (FirebaseAuth.instance.currentUser != null)
+            {
+              print("logged"),
+              // Future.delayed(const Duration(seconds: 1), () {
+              //   Navigator.pushReplacementNamed(context, '/gate');
+              // }),
+              SocketManager.shared.socket?.once("getUser", (data) {
+                user.customerID = data["customerID"];
+                user.accounts.clear();
+                for (var i = 0; i < data["accounts"].length; i++) {
+                  user.accounts.add(Accounts(
+                      data["accounts"][i]["id"],
+                      data["accounts"][i]["name"],
+                      data["accounts"][i]["customer_id"],
+                      int.parse(data["accounts"][i]["balance"]),
+                      data["accounts"][i]["account_status"],
+                      data["accounts"][i]["account_type"],
+                      data["accounts"][i]["currency"],
+                      data["accounts"][i]["pancard_no"],
+                      data["accounts"][i]["pin"]));
+                }
+                ;
+                Navigator.pushReplacementNamed(context, '/gate');
+              }),
+              SocketManager.shared.socket
+                  ?.emit("getUser", FirebaseAuth.instance.currentUser?.uid),
+            }
+          else
+            {
+              Future.delayed(const Duration(seconds: 1), () {
+                Navigator.pushReplacementNamed(context, '/gate');
+              }),
+            }
+          // Future.delayed(const Duration(seconds: 2), () {
+          //   Navigator.pushReplacementNamed(context, '/gate');
+          // }),
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(status),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Icon(
+                  Iconsax.strongbox_2,
+                  color: Color.fromRGBO(52, 90, 251, 1),
+                  size: 60,
+                ),
+                const Text(
+                  "EasyFin",
+                  style: TextStyle(
+                      fontFamily: "Poppins",
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic),
+                )
+              ]),
+        ),
+      ),
+    );
+  }
+}
