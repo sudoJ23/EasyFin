02a80f0768606ee6c8095e1856dd043dcd83baba
Hanggoro <65228679+Zinc4@users.noreply.github.com>
2024-01-12 09:46:06 +0700

    Create balance_card.dart

---
+++ lib/widgets/balance_card.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BalanceCard extends StatefulWidget {
+  const BalanceCard({super.key});
+
+  @override
+  State<BalanceCard> createState() => _BalanceCardState();
+}
+
+class _BalanceCardState extends State<BalanceCard> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
